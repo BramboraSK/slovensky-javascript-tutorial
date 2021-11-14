86eebabf4041a140a9d6152687a5febe5325ecf2
BramboraSK <majojo992@gmail.com>
2021-11-14 20:28:00 +0100

    Diel 3

---
+++ diel3/script.js
@@ -0,0 +1,44 @@
+// var x = 7;
+// let y = 3.14;
+// const z = "jablko";
+
+// var l;
+// l = 5;
+
+// console.log("x" + "y");
+
+// Dátové typy
+let cislo = 8;
+
+// Text piseme medzi uvodzovky, bud ', ", alebo `
+let text = "slivka";
+
+let pole = [8, 4, "hruska", 8.64];
+console.log(pole[1] + pole[3]);
+
+let objekt = {
+    jedlo: "polievka",
+    pocet: 4
+}
+
+console.log(objekt.jedlo);
+console.log(objekt.pocet);
+
+let boolean = false;
+let ostatne = [undefined, null];
+
+// Úloha
+/*
+Spravte si premennu x, ktora bude obsahovat cislo 5.
+Dalej si spravte premennu list, ktora bude obsahovat pole, ktore
+bude obsahovat hodnoty -8, -4, 0, 4, 8
+
+Scitajte premennu x s druhou hodnotou v liste a vytlacte ju do
+konzoly.
+
+Scitajte premennu x s piatou hodnotou v liste a vytlacte ju do
+konzoly.
+
+Scitajte prvu hodnotu v liste s poslednou hodnotou v liste a
+vytlacte ju do konzoly.
+*/
